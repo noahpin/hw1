3d33a10a71caf5c03566f7da1b2791c14bf9a719
Noah Pinales <pinales@usc.edu>
2024-01-24 22:02:38 -0800

    silly little bugfixing for split

--- test_split.cpp
+++ test_split.cpp
@@ -24,4 +24,66 @@ void deleteList(Node * list);
 
 int main(int argc, char *argv[])
 {
+    Node* list = makeList({5, 6, 7, 8, 9, 10, 72, 256, 256, 1111, 1111, 1111, 9009});
+	Node* odds = nullptr;
+	Node* evens = nullptr;
+
+	split(list, odds, evens);
+
+    cout << "firstOdd" << odds->value << endl;
+    
+    Node* curr = odds;
+    while(curr != nullptr) {
+      cout << curr->value << ", " << endl;
+      curr = curr->next;
+    }
+    cout << "firstEven" << evens->value << endl;
+    curr = evens;
+    while(curr != nullptr) {
+      cout << curr->value << ", " << endl;
+      curr = curr->next;
+    }
+
+    deleteList(odds);
+    deleteList(evens);
+}
+
+Node *makeList(std::vector<int> const &content)
+{
+    Node *head = nullptr;
+
+    if (!content.empty())
+    {
+        // create head
+        head = new Node(content[0], nullptr);
+
+        // create rest of list
+        Node *currTail = head;
+        std::vector<int>::const_iterator nextValIter(content.cbegin());
+        ++nextValIter;
+
+        for (; nextValIter != content.cend(); ++nextValIter)
+        {
+            Node *newNode = new Node(*nextValIter, nullptr);
+            currTail->next = newNode;
+
+            currTail = newNode;
+        }
+    }
+
+    return head;
+}
+
+void deleteList(Node * list)
+{
+	Node * currElement = list;
+
+	while(currElement != nullptr)
+	{
+		Node * nextElement = currElement->next;
+
+		delete currElement;
+
+		currElement = nextElement;
+	}
 }
